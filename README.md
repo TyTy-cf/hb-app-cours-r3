5acad11859d59a35f4a1bf58254db45e6334e47e
TOURRET Kevin <tourret.kevin@gmail.com>
2021-07-19 16:36:11 +0200

    Correction - 19/07 + suite exos

--- README.md
+++ README.md
@@ -3,11 +3,14 @@
 ### 1.
 A partir d’un âge, on doit indiquer l’année de naissance
 
+
 ### 2.
 A partir de ce tableau [ 12, 15, 19, 2], calculer la moyenne des valeurs.
 
+
 ### 3.
 A partir d’un prix HT unitaire d’un produit, ainsi que le nombre de produit. On veut connaitre le total TTC arrondi à 2 chiffres après la virgule.
+
 
 ### 4.
 A partir de la température d’un volume d’eau, on veut savoir dans quel état est l’eau (solide, liquide ou gaz) (positif : liquide, négatif : solide, au dela de 70° [indicatif] : gaz)
@@ -23,14 +26,17 @@ Michel : 14, 13, 12, 11, 10
 Vincent : 17, 16, 15, 18, 13
 Faite une fonction qui prend en paramètre un tableau de note et permet de calculer la moyenne de l'étudiant
 
+
 ### 6.
 Faire une fonction qui prend 2 arguments en paramètres : un prix et un pourcentage.
 
 La fonction doit renvoyer le prix augmenté avec le pourcentage
 
+
 ### 7.
 
 Écrivez une fonction pour supprimer les doublons d’un tableau : Exemple : [1, 2, 2, 3, 3, 3, 4, 5, 5] Résultat attendu : [1, 2, 3, 4, 5]
+
 
 ### 8.
 
@@ -40,6 +46,7 @@ Avec le moins de lignes de code possible, proposez moi un algorithme qui, pour u
 1x2 = 2
 etc Et ce jusqu'à 12
 
+
 ### 9.
 
 Faites une fonction qui prend en argument une chaîne de caractères (LONGUE) Cette fonction doit afficher les 15 premiers caractères puis '...' Par exemple :
@@ -47,9 +54,42 @@ Faites une fonction qui prend en argument une chaîne de caractères (LONGUE) Ce
 Je passe la chaîne : 'Lorem quisque class vestibulum'
 La fonction doit afficher 'Lorem quisque c...'
 
+
 ### 10.
 
 Faites une fonction checkPassword, dont le but est de vérifier la validité d'un mot de passe, qui sera pris en argument Un mot de passe est considéré valide lorsqu'il fait plus de 9 caractères, et qu'il contient au moins le caratère '@' La fonction renverra un booléen pour indiquer la validité du mot de passe
 
+
+### 11.
+
+Faire une fonction qui prend en paramètre une durée, en miilisecondes
+
+Et l'afficher sous forme de chaîne de caratères
+
+Exemple :
+
+Param : 225000
+
+Affichage : 03:45"00
+
+
+### 12.
+
+Faire une fonction qui ajoute derrière chaque voyelle d'une chaine de caractères 'fe' et répète ensuite la voyelle
+
+Par exemple :
+
+Chat donne : chafeat
+
+### 13.
+
+Créer une interface Boisson, elle est caractérisée par :
+- name
+- alcohol : oui ou non
+- price
+
+Créer une classe Bar, elle est caractérisée par un tableau de boisson, qui représente la carte du bar
+Un bar a aussi un nom
+Faites une fonction, dans la classe Bar, qui permet d'afficher la "carte" du bar en console.log
 
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import {User} from "./model/user";
-import {Car} from "./model/car";
+import {User} from "../model/user";
+import {Car} from "../model/car";
+import {Student} from "../model/student";
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,7 @@ export class AppComponent {
   // public year: number;
   public user: User;
   public car: Car;
+  public students: Student[];
 
   constructor() {
     const piou = 'Jhon';
@@ -25,14 +27,100 @@ export class AppComponent {
       firstName: 'Kevin',
     };
     this.user = user;
-
     this.car = new Car();
     this.car.brand = 'Audi';
-    this.exempleExoUn();
+
+    // Exo 1 :
+    this.exoUn(42);
+
+    // Exo 2 :
+    this.exoDeux([ 12, 15, 19, 2]);
+
+    // Exo 3 :
+    this.exoTrois(20, 3);
+
+    // Exo 4 :
+    this.exoQuatre(-10);
+
+    // Exo 5 :
+    this.students = [
+      {
+        name: 'Albert',
+        notes: new Array<number>(12, 8, 9, 7, 13),
+      },
+      {
+        name: 'Michel',
+        notes: new Array<number>(14, 13, 12, 11, 10),
+      },
+      {
+        name: 'Vincent',
+        notes: new Array<number>(17, 16, 15, 18, 13),
+      },
+    ];
+    this.exoCinq();
+
+    // Exo 6 :
+    this.exoSix();
   }
 
-  exempleExoUn(): void {
-    console.log('Résultat exo 1 : ');
+  private exoUn(age: number): void {
+    const currentDate = new Date();
+    const birthYear = currentDate.getFullYear() - age;
+    console.log('Résultat exo 1 pour l age : ' + age + ' est ' + birthYear);
+    // console.log('Résultat exo 1 pour l age : ' + age + ' est ' + (new Date()).getFullYear() - age);
   }
 
+  private exoDeux(numbers: number[]): void {
+    console.log('Résultat exo 2, la moyenne est de : ' +
+      this.calculateAverage(numbers))
+    ;
+  }
+
+  private calculateAverage(notes: number[]): number {
+    let sumNotes = 0;
+    for (const note of notes) {
+      sumNotes += note;
+    }
+    // numbers.forEach((note) => {
+    //   sumNotes += note;
+    // });
+    return sumNotes/notes.length;
+  }
+
+  private exoTrois(price: number, quantity: number) {
+    console.log('Résultat exo 3, prix TTC : ' +
+      this.calculateTTCPrice(price, quantity)+ '€')
+    ;
+  }
+
+  private calculateTTCPrice(price: number, quantity: number): number {
+    return (price * quantity) * 1.2;
+  }
+
+  private exoQuatre(temperature: number): void {
+    let result = temperature >= 0 ? 'liquide' : 'solide';
+    if (temperature >= 70) {
+      result = 'gaz';
+    }
+    console.log('Résultat exo 4, information de la température ' + temperature + ' : ' + result);
+  }
+
+  private exoCinq(): void {
+    console.log('Résultat exo 5 : ');
+    for (const student of this.students) {
+      console.log('La moyenne de : ' + student.name + ' est de ' +
+        this.calculateAverage(student.notes))
+      ;
+    }
+  }
+
+  private exoSix() {
+    console.log('Résultat exo 6, le prix augmenté est de : ' +
+      this.increasePrice(50, 25) + '€')
+    ;
+  }
+
+  private increasePrice(price: number, percent: number): number {
+    return Math.round(price * (1 + percent/100) * 100)/100;
+  }
 }
