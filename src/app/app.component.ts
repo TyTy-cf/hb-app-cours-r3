195df2769ead34d651ef38a746005d2919b4962f
TOURRET Kevin <tourret.kevin@gmail.com>
2021-07-19 11:12:44 +0200

    Cours Typescript 19/07

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {User} from "./model/user";
+import {Car} from "./model/car";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app-cours';
+
+  // public year: number;
+  public user: User;
+  public car: Car;
+
+  constructor() {
+    const piou = 'Jhon';
+    this.user = {
+      name: piou,
+      firstName: 'Doe',
+    };
+    this.user.name = 'Johny';
+    const user = {
+      name: 'Tourret',
+      firstName: 'Kevin',
+    };
+    this.user = user;
+
+    this.car = new Car();
+    this.car.brand = 'Audi';
+  }
+
+  variables(user: User): void {
+    const day = 1;
+
+    // this.year = 2021;
+  }
+
 }
