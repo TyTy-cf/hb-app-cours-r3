d4051530c31346e70bba0f4ec412fce5427f10af
Kevin Tourret <tarpion@toto.fr>
2021-07-28 12:13:29 +0200

    Deplacement piece ameliore

--- src/app/chess/chess.component.ts
+++ src/app/chess/chess.component.ts
@@ -19,26 +19,25 @@ export class ChessComponent implements OnInit {
 
   clickCase(clickedCase: Case) {
     // Si j'ai déjà sélectionné une case
-    if (this.selectedCase) {
-      // Que la case précédemment sélectionnée est égale à la case que je viens de cliquer
-      if (this.selectedCase.getName() === clickedCase.getName()) {
-        // Alors je réinitialise la valeur de ma case sélectionnée
-        this.selectedCase = undefined;
-        // Je remet la case que je viens de cliquer à sa couleur initiale
-        clickedCase.color = clickedCase.initialColor;
+    // Que la case précédemment sélectionnée est égale à la case que je viens de cliquer
+    if (this.selectedCase?.getName() === clickedCase.getName()) {
+      // Alors je réinitialise la valeur de ma case sélectionnée
+      this.selectedCase = undefined;
+      // Je remet la case que je viens de cliquer à sa couleur initiale
+      clickedCase.color = clickedCase.initialColor;
+    } else {
+      // Si la case cliquée a une piece
+      if (clickedCase.piece) {
+        // Si l'on avait précédemment sélectionné une
+        if (this.selectedCase) {
+          // Alors on réinitialise sa couleur
+          this.selectedCase.color = this.selectedCase.initialColor;
+        }
+        // On change la couleur de la case cliquée
+        clickedCase.color = 'selected-case';
+        // Et notre attribut de case sélectionnée devient la case cliquée
+        this.selectedCase = clickedCase;
       }
-    }
-    // Si la case cliquée a une piece
-    if (clickedCase.piece) {
-      // Si l'on avait précédemment sélectionné une
-     if (this.selectedCase) {
-       // Alors on réinitialise sa couleur
-       this.selectedCase.color = this.selectedCase.initialColor;
-     }
-     // On change la couleur de la case cliquée
-      clickedCase.color = 'selected-case';
-      // Et notre attribut de case sélectionnée devient la case cliquée
-      this.selectedCase = clickedCase;
     }
     // S'il y a bien une piece sur cette case, alors
     //        si je clique sur une autre case
