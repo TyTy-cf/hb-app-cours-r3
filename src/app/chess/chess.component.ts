1fc4c176049a1e1961399339c2891d778f41744b
Kevin Tourret <tarpion@toto.fr>
2021-07-29 09:20:05 +0200

    Improve chess component

--- src/app/chess/chess.component.ts
+++ src/app/chess/chess.component.ts
@@ -1,22 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {ChessBoard} from "../../model/chess/chessBoard";
-import {Case} from "../../model/chess/case";
 
 @Component({
   selector: 'app-chess',
   templateUrl: './chess.component.html',
   styleUrls: ['./chess.component.scss']
 })
-export class ChessComponent implements OnInit {
+export class ChessComponent {
 
   chessBoard: ChessBoard = new ChessBoard();
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  clickCase(clickedCase: Case) {
-    this.chessBoard.movePiece(clickedCase);
-  }
 }
